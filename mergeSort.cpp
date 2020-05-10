a77563327af49e33362bcfc41bc4f52e7feef11c
dhrush <dhrush97@gmail.com>
2020-05-10 12:34:55 +0530

    Merge Sort

---
+++ mergeSort.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+
+void merge(int a[], int l, int m, int r)
+{
+    int n1 = m-l+1;
+    int n2 = r-m;
+    
+    int L[n1];
+    int R[n2];
+    
+    for(int i=0; i<n1; i++)
+        L[i]=a[l+i];
+    for(int j=0; j<n2; j++)
+        R[j]=a[m+1+j];
+        
+    int k=l;
+    int i=0,j=0;
+    
+    while(i<n1 && j<n2)
+    {
+        if(L[i] <= R[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while(i<n1)
+        {
+            a[k]=L[i];
+            k++;
+            i++;
+        }
+    while(j<n2)
+        {
+            a[k]=R[j];
+            k++;
+            j++;
+        }
+}
+
+void ms(int a[], int l, int r)
+{
+    if(l<r)
+    {
+        int m = l+(r-l)/2;
+        ms(a, l, m);
+        ms(a, m+1, r);
+        merge(a, l, m, r);
+    }
+}
+
+void printA(int a[], int siz)
+{
+    for(int i=0; i<siz; i++)
+    {
+        std::cout<<a[i]<<" ";
+    }
+}
+
+
+int main()
+{
+    int a[]={6,1,3,4,5,4};
+    int sizeA = sizeof(a)/sizeof(a[0]);
+    ms(a, 0, sizeA-1);
+    printA(a, sizeA);
+    return 0;
+}
