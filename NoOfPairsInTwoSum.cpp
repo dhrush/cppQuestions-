fbf28c44c83fdae8d53729c3604a393238290953
Dhruv Gupta <dhruv3gupta@gmail.com>
2020-10-19 17:36:15 +0000

    count pairs in two sum

--- NoOfPairsInTwoSum.cpp
+++ NoOfPairsInTwoSum.cpp
@@ -1,20 +1,31 @@
-#include<iostream>
-#include<vector>
-#include<unordered_map>
-using namespace std;
-
-int main()
-{
-    vector<int> nums = {1,5,7,1};
-    int target = 6;
-    int count = 0;
-    unordered_map<int,int> imap;
-    for(auto idx = nums.begin(); idx!=nums.end(); ++idx)
-    {   
-        auto it = imap.find(target - *idx);
-        if(it!=imap.end())   count++;
-        imap[*idx] = idx-nums.begin();
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) 
+    {
+        // code here
+        unordered_map<int, int> m; 
+  
+    // Store counts of all elements in map m 
+        for (int i=0; i<n; i++) 
+            m[arr[i]]++; 
+      
+        int twice_count = 0; 
+      
+        // iterate through each element and increment the 
+        // count (Notice that every pair is counted twice) 
+        for (int i=0; i<n; i++) 
+        { 
+            twice_count += m[k-arr[i]]; 
+      
+            // if (arr[i], arr[i]) pair satisfies the condition, 
+            // then we need to ensure that the count is 
+            // decreased by one such that the (arr[i], arr[i]) 
+            // pair is not considered 
+            if (k-arr[i] == arr[i]) 
+                twice_count--; 
+        } 
+      
+        // return the half of twice_count 
+        return twice_count/2; 
     }
-    cout<<count<<"\n";
-    return 0;
-}
+};
